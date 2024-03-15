6f296b98bd15c67d7e67bdb2081ee8ed3dd2e722
Gabriel Lourenço <gabriellourenco@MacBook-Air-de-Gabriel.local>
2024-03-15 10:30:05 -0300

    feat: adiciona exercicio de ordenação

---
+++ ExercicioOrdenacao/Exe06A/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] vetor  = new int[100];
+int auxiliar;
+
+Random aleatorio = new Random();
+
+for(int i =0; i < vetor.Length; i++ ){
+    vetor[i] = aleatorio.Next(100);
+}
+
+Array.Sort(vetor);
+
+for(int i =0; i <= vetor.Length; i++ ){
+    Console.WriteLine(vetor[i] + " ");
+}
---
+++ ExercicioOrdenacao/Exe06B/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] vetor  = new int[100];
+int auxiliar;
+
+Random aleatorio = new Random();
+
+for(int i =0; i < vetor.Length; i++ ){
+    vetor[i] = aleatorio.Next(100);
+}
+
+for(int i =0; i < vetor.Length; i++ ){
+    if(vetor[i] > vetor[i + 1]){
+        auxiliar = vetor[i];
+
+        vetor[i] = vetor[i+1];
+        vetor[i+1] = auxiliar;
+    }
+}
+
+for(int i =0; i <= vetor.Length; i++ ){
+    Console.WriteLine(vetor[i] + " ");
+}
